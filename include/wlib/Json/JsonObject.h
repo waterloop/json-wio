2f3348950b78203bde770f838b28a44df5ebb4d0
Jeff Niu <jeffniu22@gmail.com>
2018-06-06 22:02:47 -0400

    Created JsonObject extending hash_map

--- include/wlib/Json/JsonObject.h
+++ include/wlib/Json/JsonObject.h
@@ -3,18 +3,36 @@
 
 #include <wlib/hash_map>
 
+#include <wlib/Json/JsonUtil.h>
+
 namespace wlp {
 
     class json_element;
-    struct json_element_hash;
-    struct json_element_equals;
 
-    typedef hash_map<
-        json_element,
-        json_element,
-        json_element_hash,
-        json_element_equals
-    > json_object;
+    class json_object :
+        public hash_map<
+            json_element,
+            json_element,
+            json_element_hash,
+            json_element_equals> {
+
+        enum {
+            JSON_OBJECT_DEFAULT_SIZE = 12,
+            JSON_OBJECT_DEFAULT_LOAD = 75
+        };
+
+    public:
+        explicit json_object(
+            size_type n = JSON_OBJECT_DEFAULT_SIZE,
+            percent_type load = JSON_OBJECT_DEFAULT_LOAD
+        );
+
+        json_object(json_object &&obj);
+        json_object(const json_object &) = delete;
+
+        json_object &operator=(json_object &&obj);
+        json_object &operator=(const json_object &) = delete;
+    };
 
 }
 
---
+++ src/wlib/Json/JsonObject.cpp
@@ -0,0 +1,17 @@
+#include <wlib/Json/JsonObject.h>
+#include <wlib/Json/JsonElement.h>
+
+using namespace wlp;
+
+json_object::json_object(
+    json_object::size_type n,
+    json_object::percent_type load) :
+    hash_map(n, load) {}
+
+json_object::json_object(json_object &&obj) : hash_map(move(obj)) {}
+
+json_object &json_object::operator=(json_object &&obj) {
+    hash_map::operator=(move(obj));
+    return *this;
+}
+
